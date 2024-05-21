91b9ee3f838274b5be048c089c985fca765e553d
Johan Magnusson <joma2304@student.miun.se>
2024-05-21 13:08:16 +0200

    Skydd för admin

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,10 +1,15 @@
-window.onload = init;
-//KOllar så att token finns för admin.html annars skickas man till login.html
+// Funktion för att kontrollera JWT-token när sidan är laddad
+document.addEventListener("DOMContentLoaded", function() {
+    init();
+});
+
 async function init() {
+    // Hämta JWT-token från localStorage
+    const jwtToken = localStorage.getItem('jwtToken');
 
-    const jwtToken = localStorage.getItem("jwtToken");
-    if(!jwtToken) { //IFall inte token finns
-        window.location.href="login.html" //Omdirigera till login sida
+    // Kontrollera om JWT-token finns
+    if (!jwtToken) {
+        // Om inte, omdirigera till login-sidan
+        window.location.href = "login.html";
     }
-
-};
+}
